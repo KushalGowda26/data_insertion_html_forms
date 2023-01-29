6d9b02bd1c1ff346aed20ca2a6e6bce0ed961dfe
KushalGowda26 <tk.kushal11@gmail.com>
2023-01-29 21:49:10 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from app.models import *
+
+def insert_topic(request):
+  if request.method=="POST":
+    tn=request.POST['topic']
+    T=Topic.objects.get_or_create(topic_name=tn)[0]
+    T.save()
+    return HttpResponse('Topic insertion done')
+  return render(request, 'insert_topic.html')
+
+def insert_webpage(request):
+  topics=Topic.objects.all()
+  d={'topic':topics}
+  if request.method=="POST":
+    topic=request.POST['topic']
+    name=request.POST['name']
+    url=request.POST['url']
+    T=Topic.objects.get_or_create(topic_name=topic)[0]
+    T.save()
+    W=Webpage.objects.get_or_create(topic_name=T, name=name, url=url)[0]
+    W.save()
+    return HttpResponse('Webpage insertion done')
+  return render(request, 'insert_webpage.html',d)
+
+
+def insert_records(request):
+  web=Webpage.objects.all()
+  d={'Web':web}
+  if request.method=="POST":
+    topic=request.POST['topic']
+    name=request.POST['name']
+    date=request.POST['date']
+    url=request.POST['url']
+    T=Topic.objects.get_or_create(topic_name=topic)[0]
+    T.save()
+    W=Webpage.objects.get_or_create(topic_name=T, name=name, url=url)[0]
+    W.save()
+    A=AccessRecords.objects.get_or_create(name=W, date=date)
+    A.save()
+    return HttpResponse('Record insertion done')
+  return render(request, 'insert_records.html',d)
+    
